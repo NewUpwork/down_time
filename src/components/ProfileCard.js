26d68c811c886b1cd221c265d9d198f3204a2dd4
Mufida Alakulju <mufidaalakulju@gmail.com>
2023-12-15 12:57:34 +0200

    typo in user name

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -38,7 +38,7 @@ const ProfileCard =  () => {
       console.error('Logout failed:', error);
     }
   };  
-  const displayedKeys = ['firstName', 'lastName', 'username', 'role'];
+  const displayedKeys = ['firstName', 'lastName', 'Username', 'role'];
 
   return (
     <div className="profile-card">
